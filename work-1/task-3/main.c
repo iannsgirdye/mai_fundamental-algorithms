a4685972a0fcba32d7ce287b5c02b1163ed96c41
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-09-19 23:26:33 +0300

    work-1/task-3: Create main part of program

--- work-1/task-3/main.c
+++ work-1/task-3/main.c
@@ -1,9 +1,22 @@
 #include "utilities.h"
+#include "functions.h"
 
 
 int main(int argc, char* argv[]) {
   if (validate_arguments_count(argc, argv) != OK) {
     return 0;
+  }
+
+  switch (argv[1][1]) {
+    case 'q':
+      flag_q(argc, argv);
+      break;
+    case 'm':
+      flag_m(argc, argv);
+      break;
+    case 't':
+      flag_t(argc, argv);
+      break;
   }
 
   return 0;
