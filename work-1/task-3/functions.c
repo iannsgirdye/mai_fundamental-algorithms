52b0c281386809f681fc800c0f5698b5c4f21d51
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-09-19 23:06:53 +0300

    work-1/task-3: Create function for "-q" flag

---
+++ work-1/task-3/functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include "utilities.h"
+#include "functions.h"
+#include "colors.h"
+
+
+void flag_q(int argc, char* argv[]) {
+  double numbers[4];
+  convert_all_double_numbers(argc, argv, numbers);
+
+  double epsilon = numbers[0];
+
+  double results[12];
+  bool repeated_results[12] = {false};
+  int found_results = 0;
+
+  for (int i = 1; i < 4; i++) {
+    double a = numbers[i];
+    for (int j = 1; j < 4; j++) {
+      if (i != j) {
+        double b = numbers[j];
+        for (int k = 1; k < 4; k++) {
+          if (i != k && j != k) {
+            double c = numbers[k];
+            
+            double D = b * b - 4 * a * c;
+            if (D >= 0) {
+              results[found_results] = (-b + sqrt(D)) / (2 * a);
+              found_results++;
+              results[found_results] = (-b - sqrt(D)) / (2 * a);
+              found_results++;
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+  printf(COLOR_BOLD_GREEN "Результат:" COLOR_WHITE);
+
+  if (!found_results) {
+    printf(" нет решений.");
+  }
+
+  for (int i = 0; i < found_results; i++) {
+    for (int j = i + 1; j < found_results; j++) {
+      if (fabs(results[i] - results[j]) < epsilon) {
+        repeated_results[j] = true;
+      }
+    }
+
+    if (!repeated_results[i]) {
+      printf(" %lf", results[i]);
+    }
+  }
+
+  printf("\n");
+}
---
+++ work-1/task-3/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include "utilities.h"
+
+// Обработка флага -q
+void flag_q(int argc, char* argv[]);
+
+#endif
