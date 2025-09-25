9bbe6e0539a5eb06d2130a96a0975f0eac1d1f8b
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-09-25 17:37:30 +0300

    work-1/task-5: Add main program file

---
+++ work-1/task-5/src/main.c
@@ -0,0 +1,45 @@
+#include "../include/utilities.h"
+#include "../include/options.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char* argv[]) {
+  if (validateCountOfArguments(argc, argv) != OK) {
+    return 0;
+  }
+
+  char mode;
+  bool haveOutputFile;
+  if (validateFlag(argv[1], &mode, &haveOutputFile) != OK) {
+    return 0;
+  }
+
+  char* inputFileName = argv[2];
+  char* outputFileName = "";
+  if (haveOutputFile) {
+    defineOutputFileName(&outputFileName, argc, argv);
+  }
+
+  switch (mode) {
+    case 'd':
+      optionD(inputFileName, outputFileName);
+      break;
+    case 'i':
+      optionI(inputFileName, outputFileName);
+      break;
+    case 's':
+      optionS(inputFileName, outputFileName);
+      break;
+    case 'a':
+      optionA(inputFileName, outputFileName);
+      break;
+  }
+
+  /* строка outputFileName хранится в динамической памяти,
+  которая выделяется в функции defineOutputFileName */
+  free(outputFileName);
+
+  return 0;
+}
