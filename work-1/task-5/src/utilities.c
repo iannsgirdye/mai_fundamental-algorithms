723fea75e3977f539ac9fb8a28ad8fa2c0024028
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-09-25 12:03:22 +0300

    work-1/task-5: Add print of errors for some utility
    Utility to validate count of arguments

--- work-1/task-5/src/utilities.c
+++ work-1/task-5/src/utilities.c
@@ -1,12 +1,23 @@
 #include "utilities.h"
+#include "colors.h"
 
 
 returnStatus validateCountOfArguments(int argc, char* argv[]) {
   if (argc < 2 || argc > 3) {
+    printf(
+      COLOR_BOLD_RED "Ошибка: "
+      COLOR_WHITE " введено некорректное количество аргументов. "
+      "(справка: флаг \"-h\")."
+    );
     return INVALID_COUNT_OF_ARGUMENTS;
   }
 
   if (argv[1][1] == 'n' && argv == 2) {
+    printf(
+      COLOR_BOLD_RED "Ошибка: "
+      COLOR_WHITE " введено некорректное количество аргументов "
+      "(справка: флаг \"-h\")."
+    );
     return INVALID_COUNT_OF_ARGUMENTS;
   }
 
