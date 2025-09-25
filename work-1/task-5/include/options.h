51785772097625d6b4d7af793e532bb3faf69868
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-09-25 15:41:56 +0300

    work-1/task-5: Add option for '-d' flag

---
+++ work-1/task-5/include/options.h
@@ -0,0 +1,11 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+
+#include "./utilities.h"
+
+
+returnStatus optionD(char inputFileName[], char outputFileName[]);
+
+
+#endif
---
+++ work-1/task-5/src/options.c
@@ -0,0 +1,38 @@
+#include "../include/options.h"
+#include "../include/colors.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+returnStatus optionD(char inputFileName[], char outputFileName[]) {
+  FILE* inputFile = fopen(inputFileName, "r");
+  if (checkFileOpening(inputFile, "входной") != OK) {
+    return INVALID_FILE;
+  }
+
+  FILE* outputFile;
+  if (strlen(outputFileName) > 0) {
+    outputFile = fopen(outputFileName, "w");
+    if (checkFileOpening(outputFile, "выходной") != OK) {
+      return INVALID_ARGUMENT;
+    }
+  } else {
+    outputFile = stdout;
+  }
+
+  char symbol;
+  while ((symbol = fgetc(inputFile)) != EOF) {
+    if (!isdigit(symbol)) {
+      fputc(symbol, outputFile);
+    }
+  }
+
+  fclose(inputFile);
+  if (strlen(outputFileName) > 0) {
+    fclose(outputFile);
+  }
+
+  printf("\n");
+  return OK;
+}
