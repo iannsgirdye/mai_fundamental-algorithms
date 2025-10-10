23e3b4217bc5b139919ea149b6e37deef8783881
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-10-10 21:50:11 +0300

    work-1/task-4: Add bold yellow color

--- work-1/task-4/include/colors.h
+++ work-1/task-4/include/colors.h
@@ -2,9 +2,10 @@
 #define COLORS_H
 
 
-#define COLOR_WHITE      "\e[0;37m"
-#define COLOR_BOLD_RED   "\e[1;31m"
-#define COLOR_BOLD_GREEN "\e[1;32m"
+#define COLOR_WHITE       "\e[0;37m"
+#define COLOR_BOLD_RED    "\e[1;31m"
+#define COLOR_BOLD_GREEN  "\e[1;32m"
+#define COLOR_BOLD_YELLOW "\e[1;33m"
 
 
 #endif
