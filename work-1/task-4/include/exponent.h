f4f1a634c98109fe18a114a93de4fc159520a8f8
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-10-10 18:56:48 +0300

    work-1/task-4: Add limit for exponent

---
+++ work-1/task-4/include/exponent.h
@@ -0,0 +1,8 @@
+#ifndef EXPONENT_H
+#define EXPONENT_H
+
+
+double calculateExponentUsingLimit(const double epsilon);
+
+
+#endif
---
+++ work-1/task-4/src/exponent.c
@@ -0,0 +1,23 @@
+#include "../include/exponent.h"
+#include <math.h>
+#include <stdlib.h>
+
+
+double _calculateExponentFunction(const int n) {
+  return pow(1 + 1.0 / n, n);
+}
+
+
+double calculateExponentUsingLimit(const double epsilon) {
+  int n = 1;
+  
+  double prevValue = _calculateExponentFunction(n);
+  double currentValue = _calculateExponentFunction(++n);
+
+  while (fabs(currentValue - prevValue) >= epsilon) {
+    prevValue = currentValue;
+    currentValue = _calculateExponentFunction(++n);
+  }
+
+  return currentValue;
+}
