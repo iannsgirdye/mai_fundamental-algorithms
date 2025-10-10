b92d0ff56ac13c098a18954c07e978730afc81d7
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-10-10 22:02:59 +0300

    work-1/task-4: Add cyan color

--- work-1/task-4/include/colors.h
+++ work-1/task-4/include/colors.h
@@ -3,6 +3,7 @@
 
 
 #define COLOR_WHITE       "\e[0;37m"
+#define COLOR_CYAN        "\e[0;36m"
 #define COLOR_BOLD_RED    "\e[1;31m"
 #define COLOR_BOLD_GREEN  "\e[1;32m"
 #define COLOR_BOLD_YELLOW "\e[1;33m"
