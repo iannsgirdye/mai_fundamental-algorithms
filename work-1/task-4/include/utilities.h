3c2eca10d6ff5b7b16e6c1e536c2207731fbf15e
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-10-11 11:48:54 +0300

    work-1/task-4: Add utilities to check type of constant

--- work-1/task-4/include/utilities.h
+++ work-1/task-4/include/utilities.h
@@ -7,9 +7,18 @@
 #define EPSILON 1e-7
 
 
+#include <stdbool.h>
+
+
 unsigned long long factorial(const int n);
 double _middleValue(const double leftValue, const double rightValue);
 double _combinations(const int k, const int m);
+
+bool _typeIsExponent(const char *type);
+bool _typeIsPi(const char *type);
+bool _typeIsLn2(const char *type);
+bool _typeIsSqrt2(const char *type);
+bool _typeIsGamma(const char *type);
 
 
 #endif
--- work-1/task-4/src/utilities.c
+++ work-1/task-4/src/utilities.c
@@ -1,4 +1,34 @@
 #include "../include/utilities.h"
+#include <stdbool.h>
+#include <string.h>
+
+
+#define STRINGS_ARE_EQUAL 0
+
+
+bool _typeIsExponent(const char *type) {
+  return strcmp(type, "exponent") == STRINGS_ARE_EQUAL;
+}
+
+
+bool _typeIsPi(const char *type) {
+  return strcmp(type, "pi") == STRINGS_ARE_EQUAL;
+}
+
+
+bool _typeIsLn2(const char *type) {
+  return strcmp(type, "ln2") == STRINGS_ARE_EQUAL;
+}
+
+
+bool _typeIsSqrt2(const char *type) {
+  return strcmp(type, "sqrt2") == STRINGS_ARE_EQUAL;
+}
+
+
+bool _typeIsGamma(const char *type) {
+  return strcmp(type, "gamma") == STRINGS_ARE_EQUAL;
+}
 
 
 unsigned long long factorial(const int n) {
