7bdde47cb6c887807b1967d1cacd7eaead7faf80
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-10-30 22:33:17 +0300

    work-2/task-1: Delete NO_ARGUMENTS return status

--- work-2/task-1/include/returnStatus.h
+++ work-2/task-1/include/returnStatus.h
@@ -4,10 +4,9 @@
 
 typedef enum {
   OK = 0,
-  NO_ARGUMENTS = 1,
-  INVALID_SYSTEM = 2,
-  INVALID_ARRAY = 3,
-  INVALID_VALUE = 4
+  INVALID_SYSTEM = 1,
+  INVALID_ARRAY = 2,
+  INVALID_VALUE = 3
 } returnStatus;
 
 
