408f2636e6ed32e72b9f668b9ea4660ac289756e
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-10-30 19:02:30 +0300

    work-2/task-1: Add return statuses

---
+++ work-2/task-1/include/returnStatus.h
@@ -0,0 +1,14 @@
+#ifndef RETURN_STATUS_H
+#define RETURN_STATUS_H
+
+
+typedef enum {
+  OK = 0,
+  NO_ARGUMENTS = 1,
+  INVALID_SYSTEM = 2,
+  INVALID_ARRAY = 3,
+  INVALID_VALUE = 4
+} returnStatus;
+
+
+#endif
