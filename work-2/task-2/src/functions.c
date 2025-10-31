3da2291d31ef347380087f967158f841a0e12138
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-10-31 15:37:20 +0300

    work-2/task-2: Add function "memcmp"

--- work-2/task-2/src/functions.c
+++ work-2/task-2/src/functions.c
@@ -1,6 +1,11 @@
 #include "../include/functions.h"
 
 
+#define FIRST_LESS_SECOND -1
+#define FIRST_EQUAL_SECOND 0
+#define FIRST_GREATER_SECOND 1
+
+
 void *memchr(const void *str, int c, size_t n) {
   const unsigned char *_str = (unsigned char *)str;
   const unsigned char _c = (unsigned char *)c;
@@ -10,4 +15,20 @@ void *memchr(const void *str, int c, size_t n) {
     }
   }
   return NULL;
+}
+
+
+int memcmp(const void *str1, const void *str2, size_t n) {
+  const unsigned char *_str1 = (unsigned char *)str1;
+  const unsigned char *_str2 = (unsigned char *)str2;
+  
+  for (size_t i = 0; i != n; ++i) {
+    if (_str1[i] < _str2[i]) {
+      return FIRST_LESS_SECOND;
+    }
+    if (_str1[i] > _str2[i]) {
+      return FIRST_GREATER_SECOND;
+    }
+  }
+  return FIRST_EQUAL_SECOND;
 }
