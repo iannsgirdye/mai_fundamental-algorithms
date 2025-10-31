229ea1a9fa4a7e0ece60d1367292dbfa4dcc65e8
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-10-31 23:02:20 +0300

    work-2/task-2: Add colors for printf()

---
+++ work-2/task-2/include/colors.h
@@ -0,0 +1,12 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+
+#define COLOR_YELLOW  "\e[0;33m"
+#define COLOR_BLUE    "\e[0;34m"
+#define COLOR_MAGENTA "\e[0;35m"
+#define COLOR_CYAN    "\e[0;36m"
+#define COLOR_WHITE   "\e[0;37m"
+
+
+#endif
