b01e58d986041ce5e1523840d68a740c36a9738f
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-10-31 15:37:41 +0300

    work-2/task-2: Fix function "memchr"

--- work-2/task-2/src/functions.c
+++ work-2/task-2/src/functions.c
@@ -8,7 +8,7 @@
 
 void *memchr(const void *str, int c, size_t n) {
   const unsigned char *_str = (unsigned char *)str;
-  const unsigned char _c = (unsigned char *)c;
+  const unsigned char _c = (unsigned char)c;
   for (size_t i = 0; i != n; ++i) {
     if (_str[i] == _c) {
       return (void *)(_str + i);
