f487e66df40b42a07eb7d4fe01037035e82a40d2
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-11-03 18:39:11 +0300

    work-2/task-3: Use return status in flag "%Zr"

--- work-2/task-3/src/flags/Zr.c
+++ work-2/task-3/src/flags/Zr.c
@@ -1,12 +1,14 @@
 #include "../../include/flags.h"
+#include "../../include/returnStatus.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 
 #define FIBONACCI_CAPACITY 50  // меньше 50 чисел Фибоначчи не превосходит INT_MAX
 
 
-size_t fibonacci(unsigned int fibonacciValues[], unsigned int maxNumber) {
+returnStatus fibonacci(unsigned int fibonacciValues[], unsigned int maxNumber, size_t *size) {
   if (maxNumber == 0) {
     return 0;
   }
@@ -18,18 +20,27 @@ size_t fibonacci(unsigned int fibonacciValues[], unsigned int maxNumber) {
   fibonacciValues[0] = 0;
   fibonacciValues[1] = 1;
   while (fibonacciValues[i - 1] <= maxNumber - fibonacciValues[i - 2]) {
+    if (fibonacciValues[i] > INT_MAX - fibonacciValues[i - 1]) {
+      return INVALID_ZECKENDORF_NUMBER;
+    }
     fibonacciValues[i] = fibonacciValues[i - 1] + fibonacciValues[i - 2];
     ++i;
   }
   fibonacciValues[i] = fibonacciValues[i - 1] + fibonacciValues[i - 2];
-  return i;
+  
+  *size = i;
+  return OK;
 }
 
 
-void flagZr(unsigned int number, char *zeckendorf) {
+returnStatus flagZr(unsigned int number, char *zeckendorf) {
   unsigned int fibonacciValues[FIBONACCI_CAPACITY];
-  size_t FibonacciValuesCount = fibonacci(fibonacciValues, number);
+  size_t FibonacciValuesCount;
+  if (fibonacci(fibonacciValues, number, FibonacciValuesCount) != OK) {
+    return INVALID_ZECKENDORF_NUMBER;
+  }
 
+  // 0-й элемент последовательности Фибоначчи не учитывается
   zeckendorf[FibonacciValuesCount] = '\0';
   zeckendorf[FibonacciValuesCount - 1] = '1';
   for (size_t i = FibonacciValuesCount - 1; i != 0; --i) {
@@ -40,5 +51,6 @@ void flagZr(unsigned int number, char *zeckendorf) {
       zeckendorf[i - 1] = '0';
     }
   }
-  // 0-й элемент последовательности Фибоначчи не учитывается
+  
+  return OK;
 }
