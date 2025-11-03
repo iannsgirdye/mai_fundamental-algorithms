86b92aa6635ab8abeaf9b53917b4bfb54cce58ac
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-11-03 23:28:30 +0300

    work-2/task-3: Replace dynamic array to static in "%Cv" flag

--- work-2/task-3/src/flags/CvCV.c
+++ work-2/task-3/src/flags/CvCV.c
@@ -24,52 +24,29 @@ char _digit(const char digit, const int isCapitalize) {
 }
 
 
-returnStatus _reallocResult(char **_result, size_t *capacity) {
-  *capacity *= 2;
-  
-  char *_tmpResult = (char *)realloc(*_result, *capacity);
-  if (_tmpResult == NULL) {
-    return INVALID_GET_MEMORY;
-  }
-  *_result = _tmpResult;
-
-  return OK;
-}
-
-
-returnStatus flagsCvCV(int number, const int system, char **result, const int isCapitalize) {
+returnStatus flagsCvCV(int number, const int system, char *result, const int isCapitalize) {
   if (system < MIN_SYSTEM || system > MAX_SYSTEM) {
     return INVALID_SYSTEM;
   }
 
-  size_t size = 0, capacity = 16;
-  char *_result = (char *)malloc(capacity);
-  if (result == NULL) {
-    return INVALID_GET_MEMORY;
-  }
-
+  size_t size = 0;
   const char sign = (number > 0) ? 1 : -1;
   number *= sign;
   if (sign == -1) {
-    _result[0] = '-';
+    result[0] = '-';
     size++;
   }
 
   while (number > 0) {
-    if (size == capacity && _reallocResult(&_result, &capacity) != OK) {
-      return INVALID_GET_MEMORY;
-    }
-    _result[size] = _digit(number % system, isCapitalize);
+    result[size] = _digit(number % system, isCapitalize);
     number /= system;
     ++size;
   }
-  _result[size] = '\0';
+  result[size] = '\0';
 
   for (size_t i = (sign == 1) ? 0 : 1; i != size / 2; ++i) {
-    swap(&_result[i], &_result[size - 1 - i]);
+    swap(&result[i], &result[size - 1 - i]);
   }
-
-  *result = _result;
 
   return OK;
 }
