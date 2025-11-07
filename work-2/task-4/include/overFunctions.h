dddf8fe40849cfb40884af8669d62246f60219e7
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-11-07 15:35:22 +0300

    work-2/task-4: Add interfaces of overFunctions

---
+++ work-2/task-4/include/overFunctions.h
@@ -0,0 +1,12 @@
+#ifndef OVER_FUNCTIONS_H
+#define OVER_FUNCTIONS_H
+
+
+#include <stdio.h>
+
+
+int oversscanf(const char *_Src, const char *_Format, ...);
+int overfscanf(FILE *_File, const char *_Format, ...);
+
+
+#endif
