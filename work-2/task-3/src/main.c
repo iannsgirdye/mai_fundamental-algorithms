cc831f323ce55ab6533fef9d845495a18fb8b864
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-11-07 19:09:50 +0300

    work-2/task-3: Update tests

--- work-2/task-3/src/main.c
+++ work-2/task-3/src/main.c
@@ -6,83 +6,90 @@
 #define BUFFER_SIZE 128
 
 
-void printTestHeading(const char *name) {
-  printf("\n===> %s <===\n", name);
+void printTestName(const char *name) {
+  printf("\n==> %s <==\n", name);
 }
 
 
 void test1() {
-  printTestHeading("test1");
-
   char result[BUFFER_SIZE];
   const char *str1 = "HELLO!";
   const char *str2 = "123abc";
   
-  printf("\ntest1-1: \n");
+  printTestName("test1-1");
   oversprintf(result, "oversprintf | %s %s\n", str1, str2);
   printf("%s", result);
 
-  printf("\ntest1-2: \n");
+  printTestName("test1-2");
   oversprintf(result, "oversprintf | %s %s\n", str1, str2);
   printf("%s", result);
 
-  printf("\ntest1-3: \n");
+  printTestName("test1-3");
   oversprintf(result, "oversprintf | %s %s\n", str1, str2);
   printf("%s", result);
   
-  printf("\ntest1-4: \n");
+  printTestName("test1-4");
   overfprintf(stdout, "overfprintf | %s %s\n", str1, str2);
 }
 
 
 void test2() {
-  printTestHeading("test2");
-
   char result[BUFFER_SIZE];
   const long long int number = 12345678;
 
-  printf("\ntest2-1: \n");
+  printTestName("test2-1");
   overfprintf(stdout, "overfprintf | %lld\n", number);
 
-  printf("\ntest2-2: \n");
+  printTestName("test2-2");
   oversprintf(result, "oversprintf | %lld\n", number);
   printf("%s", result);
 }
 
 
 void test3() {
-  printTestHeading("test3");
-
   char result[BUFFER_SIZE];
   const int number = 999;
   
-  printf("\ntest3-1: \n");
+  printTestName("test3-1");
   oversprintf(result, "oversprintf | %d: %Ro, %Zr\n", number, number, number);
   printf("%s", result);
 
-  printf("\ntest3-2: \n");
+  printTestName("test3-2");
   oversprintf(result, "oversprintf | %d: %Ro, %Zr\n", number, number, number);
   printf("%s", result);
 
-  printf("\ntest3-3: \n");
+  printTestName("test3-3");
   overfprintf(stdout, "overfprintf | %d: %Ro, %Zr\n", number, number, number);
 }
 
 
 void test4() {
-  printTestHeading("test4");
-
   char result[BUFFER_SIZE];
   const char *ospf = "oversprintf";
   const char *ofpf = "overfprintf";
   const int number = 1234;
 
-  printf("\ntest4-1: \n");
+  printTestName("test4-1");
   oversprintf(result, "%s | %d: %Ro, %Zr\n", ospf, number, number, number);
   printf("%s", result);
 
-  printf("\ntest4-2: \n");
+  printTestName("test4-2");
   overfprintf(stdout, "%s | %d: %Ro, %Zr\n", ofpf, number, number, number);
+}
+
+
+void test5() {
+  char result[BUFFER_SIZE];
+  const char *ospf = "oversprintf";
+  const char *ofpf = "overfprintf";
+  const int number = 1234;
+
+  printTestName("test5-1");
+  printf("count of printed symbols: %d\n", oversprintf(result, "%s | %d: %Ro, %Zr\n", ospf, number, number, number));
+  printf("%s", result);
+
+  printTestName("test5-2");
+  printf("count of printed symbols: %d\n", overfprintf(stdout, "%s | %d: %Ro, %Zr\n", ofpf, number, number, number));
 }
 
 
@@ -91,6 +98,7 @@ int main() {
   test2();
   test3();
   test4();
+  test5();
   
   return 0;
 }
