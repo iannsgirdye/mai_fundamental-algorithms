aab2077179782009d1c020435672e1cd9b19c6a3
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2025-11-11 19:30:35 +0300

    work-3/task-2: Add func "create_vector"

--- work-3/task-2/vector.h
+++ work-3/task-2/vector.h
@@ -13,6 +13,17 @@ typedef struct {
   void (*DeleteVoidPtr)(VECTOR_TYPE); 
 } Vector; 
 
+Vector create_vector(size_t initial_capacity, VECTOR_TYPE (*CopyFunc)(VECTOR_TYPE), void (*DeleteFunc)(VECTOR_TYPE)) { 
+  Vector vector = {.size = 0, .capacity = 0, .CopyVoidPtr = NULL, .DeleteVoidPtr = NULL};
+  vector.data = (VECTOR_TYPE *)malloc(initial_capacity * sizeof(VECTOR_TYPE));
+  if (vector.data == NULL) {
+    return vector;
+  }
+  vector.capacity = initial_capacity;
+  vector.CopyVoidPtr = CopyFunc;
+  vector.DeleteVoidPtr = DeleteFunc;
+  return vector;
+} 
 
 
 #endif
