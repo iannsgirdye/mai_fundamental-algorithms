b1f1489098df400d3b6988b90506a07a0b24fc61
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2025-11-13 23:26:57 +0300

    work-3/task-3: Add interface of Linked List

---
+++ work-3/task-3/include/LinkedList.h
@@ -0,0 +1,53 @@
+#ifndef LINKED_LIST
+#define LINKED_LIST
+
+#include <stdlib.h>
+
+typedef double LIST_TYPE;
+
+typedef struct Node {
+  double *data;
+  struct Node *prev;
+  struct Node *next;
+} Node;
+
+typedef struct {
+  Node *head; 
+  Node *tail;
+  size_t size;
+} LinkedList;
+
+// Создание пустого списка
+LinkedList create_list();
+
+// Очистка содержимого списка (удаление всех элементов) 
+void erase_list(LinkedList *list);
+
+// Полное удаление списка (освобождение ресурсов)
+void delete_list(LinkedList *list); 
+
+// Добавление элемента в конец списка
+void push_back_list(LinkedList *list, LIST_TYPE value);
+
+// Добавление элемента в начало списка
+void push_front_list(LinkedList *list, LIST_TYPE value);
+
+// Удаление элемента с конца списка 
+double pop_back_list(LinkedList *list);
+
+// Удаление элемента с начала списка
+double pop_front_list(LinkedList *list);
+
+// Вставка элемента по индексу
+void insert_at_list(LinkedList *list, size_t index, LIST_TYPE value);
+
+// Удаление элемента по индексу
+void delete_at_list(LinkedList *list, size_t index);
+
+// Получение элемента по индексу
+double get_at_list(const LinkedList *list, size_t index);
+
+// Сравнение двух списков (лексикографически): возвращает `1` — равны, `0` — не равны
+int is_equal_list(const LinkedList *l1, const LinkedList *l2);
+
+#endif
