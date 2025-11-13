d497885e7cb9ca13002463da871389ab9d3655bf
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2025-11-13 14:19:47 +0300

    work-3/task-1: Add utility to get base pow

---
+++ work-3/task-1/include/utilities.h
@@ -0,0 +1,8 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include "./returnStatus.h"
+
+returnStatus getBasePow(int *basePow);
+
+#endif
---
+++ work-3/task-1/src/utilities.c
@@ -0,0 +1,16 @@
+#include "../include/utilities.h"
+#include "../include/returnStatus.h"
+#include <math.h>
+
+#define MIN_BASE_POW 1
+#define MAX_BASE_POW 5
+
+returnStatus getBasePow(int *basePow) {
+  int tmpBasePow;
+  scanf("%d", &tmpBasePow);
+  if (tmpBasePow < MIN_BASE_POW || tmpBasePow > MAX_BASE_POW) {
+    return INVALID_BASE_POW;
+  }
+  *basePow = tmpBasePow;
+  return OK;
+}
